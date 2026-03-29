7a401eb76a92dca5f88067353474825029f1b612
KimJayhyun <dnlwjem000@gmail.com>
2026-03-29 14:51:47 +0900

    feat: create user endpoint

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,8 +1,49 @@
-from fastapi import APIRouter
+from typing import Annotated
 
-router = APIRouter(prefix="/auth", tags=["auth"])
+import database
+from fastapi import APIRouter, Depends
+from models import Users
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
+
+router = APIRouter(tags=["auth"])
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-@router.get("/")
-async def get_user():
-    return {"user": "authenticated"}
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        username=create_user_request.username,
+        email=create_user_request.email,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_activate=True,
+    )
+
+    db.add(create_user_model)
+    db.commit()
