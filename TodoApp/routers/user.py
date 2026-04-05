89c5af2f91f104f8a5cd4c9871f028635bd038d6
KimJayhyun <dnlwjem000@gmail.com>
2026-04-05 15:17:42 +0900

    feat: finish assignment

--- TodoApp/routers/user.py
+++ TodoApp/routers/user.py
@@ -31,6 +31,11 @@ class UserVerification(BaseModel):
     new_password: str
 
 
+class UserChangePhoneNumber(BaseModel):
+    password: str
+    new_phone_number: str
+
+
 @router.get("/", status_code=status.HTTP_200_OK)
 async def get_user(user: user_dependency, db: db_dependency):
     if user is None:
@@ -62,6 +67,33 @@ async def change_password(
         )
 
     user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+
+    db.add(user_model)
+    db.commit()
+
+
+@router.put("/phone_number", status_code=status.HTTP_204_NO_CONTENT)
+async def change_phone_number(
+    user: user_dependency,
+    db: db_dependency,
+    user_change_phone_number: UserChangePhoneNumber,
+):
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed"
+        )
+
+    user_model = db.query(Users).filter(Users.id == user.get("id")).first()
+
+    if not bcrypt_context.verify(
+        user_change_phone_number.password, user_model.hashed_password
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Error on phone number change",
+        )
+
+    user_model.phone_number = user_change_phone_number.new_phone_number
 
     db.add(user_model)
     db.commit()
