325aff6443ed8151e201f8ad12fdad08c60911f3
KimJayhyun <dnlwjem000@gmail.com>
2026-04-07 12:41:00 +0900

    test: admin endpoint

--- TodoApp/test/test_admin.py
+++ TodoApp/test/test_admin.py
@@ -1,6 +1,7 @@
-from test.utils import client
+from test.utils import TestingSessionLocal, client
 
 from fastapi import status
+from models import Todos
 
 
 def test_admin_read_all_authenticated(test_todo):
@@ -19,3 +20,21 @@ def test_admin_read_all_authenticated(test_todo):
             "owner_id": 1,
         }
     ]
+
+
+def test_admin_test_delete_todo(test_todo):
+    response = client.delete("/admin/todo/1")
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+
+    assert model is None
+
+
+def test_admin_test_delete_todo(test_todo):
+    response = client.delete("/admin/todo/999")
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {"detail": "Todo not found"}
